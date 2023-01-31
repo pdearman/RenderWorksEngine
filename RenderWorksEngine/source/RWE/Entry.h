b6dd93322de4e816f287da96f0a12f5880745d38
M. Patrick Dearman <41176541+pdearman@users.noreply.github.com>
2023-01-30 19:10:09 -0800

    Added premake support

--- RenderWorksEngine/source/RWE/Entry.h
+++ RenderWorksEngine/source/RWE/Entry.h
@@ -8,9 +8,9 @@ int main(int argc, char** argv)
 {
 
 	RWE::Log::Init();
-	RWE_CORE_WARN("Initialized spdlog \"CoreLogger\"...");
-	int a = 7;
-	RWE_INFO("Initialized spdlog \"ClientLogger\"... Var={0}", a);
+	RWE_CORE_INFO("Initialized spdlog CoreLogger...");
+	int a = 5;
+	RWE_INFO("Initialized spdlog ClientLogger...Var={0}", a);
 
 	auto RWEApp = RWE::CreateRWEApp();
 	RWEApp->Run();
---
+++ premake5.lua
@@ -0,0 +1,141 @@
+workspace "RenderWorksEngine"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+	outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+	project "RenderWorksEngine"
+		location "RenderWorksEngine"
+		kind "SharedLib"
+		language "C++"
+
+		targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+		objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+		files
+		{
+			"%{prj.name}/source/**.h",
+			"%{prj.name}/source/**.cpp",
+		}
+
+		includedirs
+		{
+			"%{prj.name}/tplibs/spdlog/include",
+		}
+
+		filter "system:windows"
+			cppdialect "C++17"
+			staticruntime "On"
+			systemversion "latest"
+
+			defines
+			{
+				"RWE_PLATFORM_WINDOWS",
+				"RWE_BUILD_DLL",
+				--"WIN_DLL",
+			}
+
+			postbuildcommands
+			{
+				("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/RWEGame")
+			}
+
+		filter "configurations:Debug"
+			defines
+			{
+				"RWE_DEBUG",
+				"_DEBUG",
+				"_CONSOLE",
+			}
+			symbols "On"
+
+		filter "configurations:Release"
+			defines
+			{
+				"RWE_RELEASE",
+				"NDEBUG",
+				"_CONSOLE",
+			}
+			optimize "On"
+
+		filter "configurations:Dist"
+			defines
+			{
+				"RWE_DIST",
+				"NDEBUG",
+				--"_CONSOLE",
+			}
+			optimize "On"
+
+		--[[filters { "system:windows", "configurations:release" }
+			buildoptions "/MT"]]--
+
+
+	project "RWEGame"
+		location "RWEGame"
+		kind "ConsoleApp"
+		language "C++"
+
+		targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+		objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+		files
+		{
+			"%{prj.name}/source/**.h",
+			"%{prj.name}/source/**.cpp",
+		}
+
+		includedirs
+		{
+			"RenderWorksEngine/tplibs/spdlog/include",
+			"RenderWorksEngine/source",
+		}
+
+		links
+		{
+			"RenderWorksEngine"
+		}
+
+		filter "system:windows"
+			cppdialect "C++17"
+			staticruntime "On"
+			systemversion "latest"
+
+			defines
+			{
+				"RWE_PLATFORM_WINDOWS",
+				--"WIN_DLL",
+			}
+
+		filter "configurations:Debug"
+			defines
+			{
+				"RWE_DEBUG",
+				"_DEBUG",
+				"_CONSOLE",
+			}
+			symbols "On"
+
+		filter "configurations:Release"
+			defines
+			{
+				"RWE_RELEASE",
+				"NDEBUG",
+				"_CONSOLE",
+			}
+			optimize "On"
+
+		filter "configurations:Dist"
+			defines
+			{
+				"RWE_DIST",
+				"NDEBUG",
+				--"_CONSOLE",
+			}
+			optimize "On"
